842fc8d7c5cadb011ed82c15b71b72088cf02647
astronomicle <719168@student.dusd.net>
2024-04-12 15:19:39 -0700

    did 1 thing
    i am on adding agents now

--- GoalDrivenBehavior/Assets/Scripts/GOAP/GAgent.cs
+++ GoalDrivenBehavior/Assets/Scripts/GOAP/GAgent.cs
@@ -48,8 +48,10 @@ public class GAgent : MonoBehaviour
     {
         if(currentAction != null && currentAction.running)
         {
-            if(currentAction.agent.hasPath && currentAction.agent.remainingDistance < 1f)
+            float distanceToTarget = Vector3.Distance(currentAction.target.transform.position, this.transform.position);
+            if(currentAction.agent.hasPath && distanceToTarget < 2f)
             {
+                Debug.Log("Distance to Goal: " + currentAction.agent.remainingDistance);
                 if (!invoked)
                 {
                     Invoke("CompleteAction", currentAction.duration);
